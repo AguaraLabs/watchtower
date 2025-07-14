46d634b861d626ab0a106250bd61d7a847470ba8
Aguaralabs <accounts@aguaralabs.com>
2025-07-14 18:35:23 -0300

    fix: change namespaces in router

--- src/routes.php
+++ src/routes.php
@@ -10,9 +10,9 @@ Route::group( [
 	|	Permission Routes
 	|-------------------------------------------------------------------------
 	*/
-	Route::get('watchtower/permission/role/{role}/edit', 'Smarch\Watchtower\Controllers\PermissionController@editRole')->name( config('watchtower.route.prefix') . 'permission.role.edit');
-	Route::post('watchtower/permission/role/{role}', 'Smarch\Watchtower\Controllers\PermissionController@updateRole')->name( config('watchtower.route.prefix') . 'permission.role.update');
-	Route::resource('watchtower/permission', 'Smarch\Watchtower\Controllers\PermissionController',
+	Route::get('watchtower/permission/role/{role}/edit', 'Aguaralabs\Watchtower\Controllers\PermissionController@editRole')->name( config('watchtower.route.prefix') . 'permission.role.edit');
+	Route::post('watchtower/permission/role/{role}', 'Aguaralabs\Watchtower\Controllers\PermissionController@updateRole')->name( config('watchtower.route.prefix') . 'permission.role.update');
+	Route::resource('watchtower/permission', 'Aguaralabs\Watchtower\Controllers\PermissionController',
 		['names' => [
     		'create'	=> config('watchtower.route.prefix') . 'permission.create',
     		'destroy'	=> config('watchtower.route.prefix') . 'permission.destroy',
@@ -31,13 +31,13 @@ Route::group( [
 	|	Role Routes
 	|-------------------------------------------------------------------------
 	*/
-	Route::get('watchtower/role/matrix', 'Smarch\Watchtower\Controllers\RoleController@showRoleMatrix')->name( config('watchtower.route.prefix') . 'role.matrix');
-	Route::post('watchtower/role/matrix', 'Smarch\Watchtower\Controllers\RoleController@updateRoleMatrix')->name( config('watchtower.route.prefix') . 'role.matrix');
-	Route::get('watchtower/role/permission/{role}/edit', 'Smarch\Watchtower\Controllers\RoleController@editRolePermissions')->name( config('watchtower.route.prefix') . 'role.permission.edit');
-	Route::post('watchtower/role/permission/{role}', 'Smarch\Watchtower\Controllers\RoleController@updateRolePermissions')->name( config('watchtower.route.prefix') . 'role.permission.update');
-	Route::get('watchtower/role/user/{role}/edit', 'Smarch\Watchtower\Controllers\RoleController@editRoleUsers')->name( config('watchtower.route.prefix') . 'role.user.edit');
-	Route::post('watchtower/role/user/{role}', 'Smarch\Watchtower\Controllers\RoleController@updateRoleUsers')->name( config('watchtower.route.prefix') . 'role.user.update');
-	Route::resource('watchtower/role', 'Smarch\Watchtower\Controllers\RoleController',
+	Route::get('watchtower/role/matrix', 'Aguaralabs\Watchtower\Controllers\RoleController@showRoleMatrix')->name( config('watchtower.route.prefix') . 'role.matrix');
+	Route::post('watchtower/role/matrix', 'Aguaralabs\Watchtower\Controllers\RoleController@updateRoleMatrix')->name( config('watchtower.route.prefix') . 'role.matrix');
+	Route::get('watchtower/role/permission/{role}/edit', 'Aguaralabs\Watchtower\Controllers\RoleController@editRolePermissions')->name( config('watchtower.route.prefix') . 'role.permission.edit');
+	Route::post('watchtower/role/permission/{role}', 'Aguaralabs\Watchtower\Controllers\RoleController@updateRolePermissions')->name( config('watchtower.route.prefix') . 'role.permission.update');
+	Route::get('watchtower/role/user/{role}/edit', 'Aguaralabs\Watchtower\Controllers\RoleController@editRoleUsers')->name( config('watchtower.route.prefix') . 'role.user.edit');
+	Route::post('watchtower/role/user/{role}', 'Aguaralabs\Watchtower\Controllers\RoleController@updateRoleUsers')->name( config('watchtower.route.prefix') . 'role.user.update');
+	Route::resource('watchtower/role', 'Aguaralabs\Watchtower\Controllers\RoleController',
 		['names' => [
     		'create'	=> config('watchtower.route.prefix') . 'role.create',
     		'destroy'	=> config('watchtower.route.prefix') . 'role.destroy',
@@ -56,11 +56,11 @@ Route::group( [
 	|	User Routes
 	|-------------------------------------------------------------------------
 	*/
-	Route::get('watchtower/user/matrix', 'Smarch\Watchtower\Controllers\UserController@showUserMatrix')->name( config('watchtower.route.prefix') . 'user.matrix');
-	Route::post('watchtower/user/matrix', 'Smarch\Watchtower\Controllers\UserController@updateUserMatrix')->name( config('watchtower.route.prefix') . 'user.matrix');
-	Route::get('watchtower/user/role/{user}/edit', 'Smarch\Watchtower\Controllers\UserController@editUserRoles')->name( config('watchtower.route.prefix') . 'user.role.edit');
-	Route::post('watchtower/user/role/{user}', 'Smarch\Watchtower\Controllers\UserController@updateUserRoles')->name( config('watchtower.route.prefix') . 'user.role.update');
-	Route::resource('watchtower/user', 'Smarch\Watchtower\Controllers\UserController',
+	Route::get('watchtower/user/matrix', 'Aguaralabs\Watchtower\Controllers\UserController@showUserMatrix')->name( config('watchtower.route.prefix') . 'user.matrix');
+	Route::post('watchtower/user/matrix', 'Aguaralabs\Watchtower\Controllers\UserController@updateUserMatrix')->name( config('watchtower.route.prefix') . 'user.matrix');
+	Route::get('watchtower/user/role/{user}/edit', 'Aguaralabs\Watchtower\Controllers\UserController@editUserRoles')->name( config('watchtower.route.prefix') . 'user.role.edit');
+	Route::post('watchtower/user/role/{user}', 'Aguaralabs\Watchtower\Controllers\UserController@updateUserRoles')->name( config('watchtower.route.prefix') . 'user.role.update');
+	Route::resource('watchtower/user', 'Aguaralabs\Watchtower\Controllers\UserController',
 		['names' => [
     		'create'	=> config('watchtower.route.prefix') . 'user.create',
     		'destroy'	=> config('watchtower.route.prefix') . 'user.destroy',
@@ -79,6 +79,6 @@ Route::group( [
 	|	Watchtower Interface Routes
 	|-------------------------------------------------------------------------
 	*/
-	Route::get('watchtower', 'Smarch\Watchtower\Controllers\WatchtowerController@index')->name( config('watchtower.route.prefix') . 'index');
+	Route::get('watchtower', 'Aguaralabs\Watchtower\Controllers\WatchtowerController@index')->name( config('watchtower.route.prefix') . 'index');
 
 });
